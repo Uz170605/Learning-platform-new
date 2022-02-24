af04bc544ddc5cd685024a0ae74a9f93ec8f98ab
Zahriddin5179 <zakhiriddinkhasanov013@gmail.com>
2022-02-24 14:21:32 +0300

    commit for to get Login classes

--- src/main/java/uz/pdp/controller/UserRoleController.java
+++ src/main/java/uz/pdp/controller/UserRoleController.java
@@ -2,11 +2,23 @@ package uz.pdp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
 @Controller
-@RequestMapping("userPanel")
-
-
+@RequestMapping("/userPanel")
 public class UserRoleController {
+    @GetMapping
+    public String loginUser (HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String userName = (String) session.getAttribute("userName");
+
+
+
+        return "";
+    }
 }
